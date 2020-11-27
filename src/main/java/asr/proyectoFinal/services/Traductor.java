77ccd0e70f80cde5363dea02e3fe47ac375078c7
alber <albertocolino4j98@gmail.com>
2020-11-27 15:02:10 +0100

    traduc24

--- src/main/java/asr/proyectoFinal/services/Traductor.java
+++ src/main/java/asr/proyectoFinal/services/Traductor.java
@@ -23,7 +23,7 @@ public class Traductor
 		else
 			model="en-es";
 		
-		Authenticator authenticator = new IamAuthenticator(claveAPI);
+		Authenticator authenticator = new IamAuthenticator("xoqZJBPpspqUhjJ3esGEs_IMoZZQpPZoYcS_2eWadlFm");
 		LanguageTranslator languageTranslator = new LanguageTranslator("2018-05-01",authenticator);
 		languageTranslator.setServiceUrl("https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/039169d7-53fc-41ae-98d1-48a3a6d26983");
 		TranslateOptions translateOptions = new TranslateOptions.Builder()
@@ -32,11 +32,13 @@ public class Traductor
 		 .build();
 		
 		TranslationResult translationResult = languageTranslator.translate(translateOptions).execute().getResult();
+		
 		System.out.println(translationResult);
+		
 		String traduccionJSON = translationResult.toString();
-		JsonParser aux = new JsonParser();
-		JsonObject rootObj = aux.parse(traduccionJSON).getAsJsonObject();
-		//JsonParser.parseString(traduccionJSON).getAsJsonObject();
+		//JsonParser aux = new JsonParser();
+		//JsonObject rootObj = aux.parse(traduccionJSON).getAsJsonObject();
+		JsonObject rootObj = JsonParser.parseString(traduccionJSON).getAsJsonObject();
 		
 		JsonArray traducciones = rootObj.getAsJsonArray("translations");
 		String traduccionPrimera = palabra;
